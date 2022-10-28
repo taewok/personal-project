50d4497276b95bddc1f13cce680b7383671bc2e6
taewok <taewok5163@naver.com>
2022-10-28 15:48:20 +0900

    경기도 휴게 음식점 개발 시작

---
+++ gyeonggido-Restarea/src/App.js
@@ -0,0 +1,14 @@
+import {useEffect} from "react";
+import styled from "styled-components";
+
+function App() {
+  useEffect(()=>{
+
+  })
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
--- movie/src/App.js
+++ movie/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { useEffect } from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+useEffect(()=>{
+  axios.get(`https://openapi.naver.com/v1/search/movie.json?X-Naver-Client-Id=tZnlIXFC0z2_iK8MWgnt&X-Naver-Client-Secret=frJe8UKSOI`).then((data)=>{
+    console.log(data.data)
+  })
+})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }

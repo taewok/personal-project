f6fdd314cc1db1c084becbe9e7e017409dab6082
taewok <taewok5163@naver.com>
2022-11-06 21:58:33 +0900

    nav 메뉴바 완성

---
+++ Yami-Recipe/src/Component/Nav.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+const Nav = () => {
+  return (
+    <NavDiv>
+      <MenuUl>
+        <MenuItem>Menu1</MenuItem>
+        <MenuItem>Menu2</MenuItem>
+        <MenuItem>Menu3</MenuItem>
+        <MenuItem>Menu4</MenuItem>
+      </MenuUl>
+    </NavDiv>
+  );
+};
+
+const NavDiv = styled.nav`
+  width: 100%;
+  height: 50px;
+  margin-top: 20px;
+  background-color: #69b645;
+  border-bottom: 3px solid #FF7A00;
+`;
+const MenuUl = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 900px;
+  height: 100%;
+  margin: 0 auto;
+  padding: 0;
+  list-style: none;
+`;
+const MenuItem = styled.li`
+    font-size: 22px;
+    color: white;
+    font-weight: 400;
+    cursor: pointer;
+    &:hover{
+        color:#FAFF00;
+    }
+`;
+
+export default Nav;

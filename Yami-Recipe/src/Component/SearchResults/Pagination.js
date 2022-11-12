5b8c81fe323b9c30caa14558ddbc393eaca2ee07
taewok <taewok5163@naver.com>
2022-11-12 14:01:47 +0900

    이전 페이지로 이동 버튼을 눌러도 보던 페이지 번호 유지

--- Yami-Recipe/src/Component/SearchResults/Pagination.js
+++ Yami-Recipe/src/Component/SearchResults/Pagination.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const Pagination = ({ postsPerPage, totalPosts, setCurrentPageNumber }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   const pageNumbers = [];
   //총 게시물 개수와 페이지당 보여질 개수를 나누어 총페이지 수를 구한다(Math.ceil를 사용하여 결과에 반올림)
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
     pageNumbers.push(i);
   }
+
+  const onClick = (list)=>{
+    setCurrentPageNumber(list);
+    navigate(`${list}`,{state:{
+      num:list
+    }})
+  }
+  useEffect(()=>{
+    console.log(location.state.num)
+    setCurrentPageNumber(location.state.num);
+  },[location.state.num, setCurrentPageNumber])
+
   return (
     <PaginationDiv>
         <PaginationUl>
         {pageNumbers.map((list) => (
-        <PaginationLl key={list} onClick={()=>setCurrentPageNumber(list)}>{list}</PaginationLl>
+        <PaginationLl key={list} onClick={()=>onClick(list)}>{list}</PaginationLl>
       ))}
         </PaginationUl>
     </PaginationDiv>
